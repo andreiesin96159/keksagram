7cfd7fdaa5b90e9be7127002c0e06300fbd9b160
Andrey <andreiesin96159@gmail.com>
2022-03-03 22:25:57 +0200

    fix js2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function randomNumber(min, max){
   const r = Math.random()*(max-min);
-  (Math.floor(r) === 0) ?  console.log('это 0') :
-  (max > min) ? console.log('min меньшу max') :
-  (max < min) ? console.log('max меньшу min') : console.log('что то другое');
+  if(Math.floor(r) === 0) { console.log('это 0') }
+ if (max > min) { console.log('min меньшу max') }
+ if (max < min) { console.log('max меньшу min') }
 }
 
 randomNumber(0, 10);
